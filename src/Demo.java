a7f056a70777c1f29ce36c37b14ff532486b1f60
ErandikaPiumali <erandipiumali99@gmail.com>
2023-05-29 10:47:04 +0530

    Loop-for practicals

---
+++ src/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String[] args) {
+
+
+        int i = 1;
+        while (i <= 8) {
+            System.out.println("Hello World!!!");
+            i++;
+
+        }
+    }
+}
